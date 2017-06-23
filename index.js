aa6acd07caee4d0482cdfb16c3451487149f70c7
github username <github email address>
2017-06-23 17:50:36 -0400

    ugjfdkg

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ var Twit = require('twit')
 
 
 var T = new Twit({
-  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-  access_token:         process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  consumer_key:         process.env['TWITTER_CONSUMER_KEY'],
+  consumer_secret:      process.env['TWITTER_CONSUMER_SECRET'],
+  access_token:         process.env['TWITTER_ACCESS_TOKEN_KEY'],
+  access_token_secret:  process.env['TWITTER_ACCESS_TOKEN_SECRET'],
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
 
